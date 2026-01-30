6468775015e985cd4a2d085f879aef2e151d58e0
Ramon Garate <ramonverse@proton.me>
2026-01-30 13:43:20 +0100

    add plans

--- billing.config.ts
+++ billing.config.ts
@@ -3,23 +3,66 @@ import { defineConfig } from "stripe-no-webhooks";
 export default defineConfig({
   test: {
     plans: [
-      // // Example plan with monthly and yearly pricing:
-      // {
-      //   name: "Pro",
-      //   description: "Access to all features",
-      //   price: [
-      //     { amount: 2000, currency: "usd", interval: "month" }, // $20/mo
-      //     { amount: 20000, currency: "usd", interval: "year" }, // $200/yr (17% savings)
-      //   ],
-      //   // Optional: credits renew each billing cycle
-      //   // Yearly subscribers get 12x monthly allocation upfront
-      //   features: {
-      //     api_calls: {
-      //       credits: { allocation: 1000 }, // 1000/mo or 12000/yr
-      //       displayName: "API Calls",
-      //     },
-      //   },
-      // },
+      {
+        id: "prod_Tt33OTGHadmPDv",
+        name: "Free",
+        description: "Cursor free plan",
+        price: [
+          {
+            id: "price_1SvGx4Hl6H1Zw3aLHjviBZJu",
+            amount: 0,
+            currency: "usd",
+            interval: "month",
+          },
+        ],
+        features: {
+          "ai-chat": {
+            credits: {
+              allocation: 1000,
+            },
+            displayName: "AI Chat",
+          },
+          "tab-completion": {
+            credits: {
+              allocation: 1000,
+            },
+            displayName: "Tab Completion",
+          },
+        },
+      },
+      {
+        id: "prod_Tt337BMhz7UN3H",
+        name: "Pro",
+        description: "Cursor Pro plan",
+        price: [
+          {
+            id: "price_1SvGx5Hl6H1Zw3aLGZNGRRBY",
+            amount: 1000,
+            currency: "usd",
+            interval: "month",
+          },
+          {
+            id: "price_1SvGx5Hl6H1Zw3aLEOgEWcsC",
+            amount: 10000,
+            currency: "usd",
+            interval: "year",
+          },
+        ],
+        features: {
+          "ai-chat": {
+            credits: {
+              allocation: 10000,
+            },
+            displayName: "AI Chat",
+          },
+          "tab-completion": {
+            credits: {
+              allocation: 10000,
+            },
+            displayName: "Tab Completion",
+          },
+        },
+      },
     ],
   },
   production: {
